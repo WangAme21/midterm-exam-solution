891d903cfeb564bc1c73216266a43143ccbb8f08
unknown <YY440@ccs.local>
2025-03-14 19:34:53 +0800

    Add Express.js

--- question3/Express.js
+++ question3/Express.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+    res.send("Welcome to my Express server!");
+});
+
 // Define the /test route
 app.get("/test", (req, res) => {
     res.json({ message: "Express is working! John Aries C. Rizada" });
